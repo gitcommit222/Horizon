cd0718d89e9940a0fc6c6351f7290ccd3ec06c1c
gitcommit222 <TiomarOne02@gmail.com>
2024-05-06 23:18:20 +0800

    page layout setup

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
+import Sidebar from "@/components/SideBar";
+
 export default function MainLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	const loggedIn = { firstName: "Marnel", lastName: "Valentin" };
 	return (
-		<main>
-			SIDEBAR
+		<main className="flex h-screen w-full font-inter">
+			<Sidebar user={loggedIn} />
 			{children}
 		</main>
 	);
